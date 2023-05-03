dcd7f6840899bd47db37fa43f2df0772d893bd4a
Krishnanand <krishnanandunnikrishnan@gmail.com>
2023-05-03 21:57:41 +0530

    added models

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,6 +1,6 @@
 const userCtrl={
     registerUser: (req,res)=>{
-        res.json({msg:"Register a User"})
+        res.json({msg:"Sign up success"})
     },
     loginUser:(req,res)=>{
         res.json({msg:"Login a User"})
