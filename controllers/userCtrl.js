77a5219c9cd016fd91b707fe1899c9146c34b410
Krishnanand <krishnanandunnikrishnan@gmail.com>
2023-05-04 07:26:04 +0530

    modified usercontroller

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,9 +1,26 @@
+const Users=require('../models/userModel')
+
 const userCtrl={
-    registerUser: (req,res)=>{
+    registerUser: async(req,res)=>{
+        try{
+        const {username,email,password}=req.body
+        const user= await Users.findOne({email:email})
+        if(user) return res.status(400).json({msg:"The email already exist."})
         res.json({msg:"Sign up success"})
+
+        } catch(err){
+            return res.status(500).json({msg:err.message})
+        }
+
     },
     loginUser:(req,res)=>{
-        res.json({msg:"Login a User"})
+        try{
+            res.json(req.body)
+            res.json({msg:"Login successful"})
+    
+            } catch(err){
+                return res.status(500).json({msg:err.message})
+            }
     }
 }
 
