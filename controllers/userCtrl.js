6a41e48245ffa90ab9b63baa2ed647db8523d7f7
Krishnanand <krishnanandunnikrishnan@gmail.com>
2023-05-06 12:41:02 +0530

    added login and signup backend

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,4 +1,6 @@
 const Users=require('../models/userModel')
+const bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
 
 const userCtrl={
     registerUser: async(req,res)=>{
@@ -6,6 +8,15 @@ const userCtrl={
         const {username,email,password}=req.body
         const user= await Users.findOne({email:email})
         if(user) return res.status(400).json({msg:"The email already exist."})
+
+        const passwordHash=await bcrypt.hash(password,10)
+
+        const newUser=new Users({
+            username:username,
+            email:email,
+            password:passwordHash
+        })
+        await newUser.save()
         res.json({msg:"Sign up success"})
 
         } catch(err){
@@ -13,10 +24,21 @@ const userCtrl={
         }
 
     },
-    loginUser:(req,res)=>{
+    loginUser:async(req,res)=>{
         try{
-            res.json(req.body)
-            res.json({msg:"Login successful"})
+            const {email,password}=req.body;
+            const user= await Users.findOne({email:email})
+            if(!user) return res.status(400).json({msg:"User doesn't exist."})
+
+            const isMatch=await bcrypt.compare(password,user.password)
+            if(!isMatch) return res.status(400).json({msg:"Incorrect password"})
+
+            //if correct create token
+            const payload = {id:user._id,name:user.username}
+            const token=jwt.sign(payload,process.env.TOKEN_SECRET,{expiresIn:"1d"})
+
+            res.json({token})
+
     
             } catch(err){
                 return res.status(500).json({msg:err.message})
